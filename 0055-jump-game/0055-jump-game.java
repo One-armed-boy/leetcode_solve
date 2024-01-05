0e0ae2eb45b1ca0870f47813229790f4dc5ad87f
이창명 <89074053+One-armed-boy@users.noreply.github.com>
2024-01-05 09:42:42 +0900

    Time: 100 ms (17.13%), Space: 44.9 MB (36.06%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[nums.length];
+        
+        queue.add(0);
+        visited[0] = true;
+        
+        while (!queue.isEmpty()) {
+            int currIdx = queue.poll();
+            
+            for (int hop = 1; hop <= nums[currIdx]; hop++) {
+                int nextIdx = currIdx + hop;
+                // System.out.println(nextIdx);
+                
+                if (nextIdx < nums.length && !visited[nextIdx]) {
+                    if (nextIdx == nums.length - 1) {
+                        return true;
+                    }
+                    queue.add(nextIdx);   
+                    visited[nextIdx] = true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
