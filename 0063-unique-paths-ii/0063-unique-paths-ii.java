751bb5f3c351ea591e982725f0bbe87fd4730f1a
이창명 <89074053+One-armed-boy@users.noreply.github.com>
2024-01-16 10:00:32 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (18.71%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    int[][] memo;
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        memo = new int[obstacleGrid.length][obstacleGrid[0].length];
+
+        for (int y = 0; y < obstacleGrid.length; y++) {
+            for (int x = 0; x < obstacleGrid[0].length; x++) {
+                memo[y][x] = -1;
+            }
+        }
+
+        memo[0][0] = 1;
+        
+        return dp(obstacleGrid.length - 1, obstacleGrid[0].length - 1, obstacleGrid);
+    }
+    
+    private int dp(int y, int x, int[][] obs) {
+        if (obs[y][x] == 1) {
+            memo[y][x] = 0;
+            return memo[y][x];
+        }
+        
+        if (y == 0 && x == 0) {
+            return memo[0][0];
+        }
+
+        if (memo[y][x] != -1) {
+            return memo[y][x];
+        }
+        
+        if (y == 0) {
+            memo[y][x] = dp(y, x-1, obs);
+            return memo[y][x];
+        }
+        
+        if (x == 0) {
+            memo[y][x] = dp(y-1, x, obs);
+            return memo[y][x];
+        }
+        
+        memo[y][x] = dp(y - 1, x, obs) + dp(y, x - 1, obs);
+        return memo[y][x];
+    }
+}
