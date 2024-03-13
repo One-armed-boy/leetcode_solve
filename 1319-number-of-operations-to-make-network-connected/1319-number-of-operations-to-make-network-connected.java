6f0e510941a152a9738bea87e705a036d4d6f590
이창명 <89074053+One-armed-boy@users.noreply.github.com>
2024-03-13 21:51:11 +0900

    Time: 118 ms (5.06%), Space: 62.5 MB (52.14%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int[] parents;
+
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) {
+            return -1;
+        }
+        parents = new int[n];
+        for (int idx = 0; idx < n; idx++) {
+            parents[idx] = idx;
+        }
+
+        for (var conn: connections) {
+            var node1 = conn[0];
+            var node2 = conn[1];
+            union(node1, node2);
+        }
+
+        HashSet<Integer> parentSet = new HashSet<>();
+        for (var parent: parents) {
+            parentSet.add(find(parent));
+        }
+
+        return parentSet.size() - 1;
+    }
+
+    private int find(int node) {
+        if (node == parents[node]) {
+            return node;
+        }
+        return find(parents[node]);
+    }
+
+    private void union(int node1, int node2) {
+        parents[find(node1)] = find(node2);
+    }
+}
