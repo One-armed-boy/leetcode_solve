31c84a110c3113e96095c5f118214731cc9b9b04
이창명 <89074053+One-armed-boy@users.noreply.github.com>
2024-03-25 09:56:48 +0900

    Time: 2693 ms (63.90%), Space: 16.4 MB (96.03%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -0,0 +1,37 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+
+        result = 0
+
+        for y in range(n):
+            for x in range(m):
+                searcher = DfsSearcher(grid)
+                result = max(result, searcher.dfs(y, x))
+
+        return result
+    
+class DfsSearcher:
+        def __init__(self, grid):
+            self.__n = len(grid)
+            self.__m = len(grid[0])
+            self.__grid = grid
+            self.__dx = [0, 0, 1, -1]
+            self.__dy = [1, -1, 0 , 0]
+
+        def dfs(self, y, x):
+            tmp = self.__grid[y][x]
+            self.__grid[y][x] = 0 
+            maxValue = 0
+            for i in range(4):
+                nextY = y + self.__dy[i]
+                nextX = x + self.__dx[i]
+                if (0 <= nextY < self.__n and 0 <= nextX < self.__m and self.__grid[nextY][nextX] > 0):
+                    maxValue = max(maxValue, self.dfs(nextY, nextX))
+                
+            self.__grid[y][x] = tmp
+            return tmp + maxValue
+            
+
+
