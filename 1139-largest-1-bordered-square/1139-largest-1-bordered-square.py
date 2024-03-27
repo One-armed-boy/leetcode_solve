022018871d025a4f458fad6a7612856ab624dd7e
이창명 <89074053+One-armed-boy@users.noreply.github.com>
2024-03-27 09:54:34 +0900

    Time: 545 ms (16.66%), Space: 16.8 MB (97.62%) - LeetHub

---
+++ 1139-largest-1-bordered-square/1139-largest-1-bordered-square.py
@@ -0,0 +1,52 @@
+class Solution:
+    def largest1BorderedSquare(self, grid: List[List[int]]) -> int:
+        self.__grid = grid
+        h = len(grid)
+        w = len(grid[0])
+        
+        oneCnt = 0
+        for y in range(h):
+            for x in range(w):
+                if grid[y][x] == 0:
+                    continue
+                oneCnt += 1
+        if oneCnt == 0:
+            return 0
+        
+        maxSide = min(h, w)
+        result = 0
+        isFind = False
+        for side in range(maxSide, 1, -1):
+            if isFind:
+                break
+            for upperY in range(h - side + 1):
+                if isFind:
+                    break
+                for leftX in range(w - side + 1):
+                    if isFind:
+                        break
+                    if self.checkSquare(upperY, leftX, side):
+                        isFind = True
+                        result = side ** 2
+        
+        return max(result, 1)
+    
+    def checkSquare(self, y, x, side):
+        return (self.checkWidth(y, x, side) 
+                and self.checkWidth(y + side - 1, x, side) 
+                and self.checkHeight(y, x, side) 
+                and self.checkHeight(y, x + side - 1, side))
+    
+    def checkWidth(self, y, x, side):
+        grid = self.__grid
+        for t in range(side):
+            if not grid[y][x + t] == 1:
+                return False
+        return True
+    
+    def checkHeight(self, y, x, side):
+        grid = self.__grid
+        for t in range(side):
+            if not grid[y + t][x] == 1:
+                return False
+        return True
