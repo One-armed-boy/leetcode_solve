09e277d9767d95c3d72b39b1b66975b01520be21
이창명 <89074053+One-armed-boy@users.noreply.github.com>
2024-04-29 09:54:51 +0900

    Time: 710 ms (48.32%), Space: 38.5 MB (12.60%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
@@ -4,7 +4,6 @@ class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
         self.numSet = set(nums)
         self.memo = {num: -1 for num in nums}
-        nums.sort()
         result = max([self.dp(num) for num in nums])
         return -1 if result < 2 else result
 
@@ -13,9 +12,10 @@ class Solution:
             return self.memo[num]
 
         tmp = math.sqrt(num)
-        if int(tmp) != tmp or (int(tmp) not in self.memo):
+        intTmp = int(tmp)
+        if intTmp != tmp or (intTmp not in self.numSet):
             self.memo[num] = 1
             return self.memo[num]
 
-        self.memo[num] = 1 + self.dp(int(tmp))
+        self.memo[num] = 1 + self.dp(intTmp)
         return self.memo[num]
