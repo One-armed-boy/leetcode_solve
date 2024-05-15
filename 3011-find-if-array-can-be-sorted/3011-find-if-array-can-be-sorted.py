589c98efd6226a6d3a6e68719a37dc6743d3a9e1
이창명 <89074053+One-armed-boy@users.noreply.github.com>
2024-05-15 09:51:45 +0900

    Time: 97 ms (37.40%), Space: 16.6 MB (25.63%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -10,11 +10,13 @@ class Solution:
             if len(summary) == 0 or summary[-1][0] != bitCount:
                 summary.append([bitCount, num, num])
                 continue
-            summary[-1] = [bitCount, min(summary[-1][1], num), max(summary[-1][2], num)]
-        
+            summary[-1][1] = min(summary[-1][1], num)
+            summary[-1][2] = max(summary[-1][2], num)
+
         for i in range(len(summary) - 1):
             if summary[i][2] > summary[i + 1][1]:
                 return False
         return True
+    
     def countBit(self, num):
         return bin(num)[2:].count("1")
