f22b2803c435ec0986a0bdb01db77ce4a1922cd7
이창명 <89074053+One-armed-boy@users.noreply.github.com>
2024-05-15 09:44:43 +0900

    Time: 85 ms (50.42%), Space: 16.6 MB (68.07%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        expandNums = [(num, self.countBit(num)) for num in nums]
+
+        # [비트 수, 최소, 최대]
+        summary = []
+
+        for i in range(len(nums)):
+            num, bitCount = expandNums[i]
+            if len(summary) == 0 or summary[-1][0] != bitCount:
+                summary.append([bitCount, num, num])
+                continue
+            summary[-1] = [bitCount, min(summary[-1][1], num), max(summary[-1][2], num)]
+        
+        for i in range(len(summary) - 1):
+            if summary[i][2] > summary[i + 1][1]:
+                return False
+        return True
+    def countBit(self, num):
+        return bin(num)[2:].count("1")
